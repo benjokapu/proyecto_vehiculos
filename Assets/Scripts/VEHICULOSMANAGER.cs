c7891876796ab66b3b7f0c3170ede5e945fd1f6f
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-06-03 08:50:40 -0300

    Package Imported and Cant Ruedas

---
+++ Assets/Scripts/VEHICULOSMANAGER.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VEHICULOSMANAGER : MonoBehaviour
+{
+
+    public Vehiculo vehiculoScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Cant ruedas: " + vehiculoScript.cantRuedas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
